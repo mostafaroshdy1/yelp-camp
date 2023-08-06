334dd67fff0adf376979aacdcd71de095bde37e3
Mostafa Roshdy <mostafaroshdy1@gmail.com>
2023-08-06 08:14:47 +0300

    Add images and description to seed and Styling

--- models/campground.js
+++ models/campground.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 
 const CampgroundSchema = new Schema({
     title: String,
+    image: String,
     price: Number,
     description: String,
     location: String
--- seeds/index.js
+++ seeds/index.js
@@ -13,9 +13,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10
         await new Campground({
             location: `${cities[random1000].city},${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/484351',
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim dolores quos fuga, vitae nihil placeat asperioresomnis unde laboriosam perspiciatis, nisi eius animi at error pariatur cum iure id eaque",
+            price: price
         }).save()
     }
 }
