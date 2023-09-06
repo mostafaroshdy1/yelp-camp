cb195b432fe27cc306eff9db8a1639d926425f23
Mostafa Roshdy <mostafaroshdy1@gmail.com>
2023-09-06 09:31:34 +0300

    Finish Section 56

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -104,6 +104,7 @@ map.on('load', function () {
     // the location of the feature, with
     // description HTML from its properties.
     map.on('click', 'unclustered-point', function (e) {
+        console.log(e)
         const { popUpMarkup } = e.features[0].properties;
         const coordinates = e.features[0].geometry.coordinates.slice();
 
--- seeds/index.js
+++ seeds/index.js
@@ -11,7 +11,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({})
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 300; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         await new Campground({
@@ -22,7 +22,10 @@ const seedDB = async () => {
             price: price,
             geometry: {
                 type: "Point",
-                coordinates: [29.894378, 31.199004]
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
             },
             images: [
                 {
