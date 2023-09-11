474a634389c6582177a1840d4967ba15679c9a35
mostafaroshdy1 <mostafaroshdy1@gmail.com>
2023-09-12 02:42:41 +0300

    Fix google auth attempt

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ const fontSrcUrls = [];
 app.use(
     helmet.contentSecurityPolicy({
         directives: {
-            formAction: ['self', 'https://yelpcampoo.onrender.com'],
+            formAction: ['self', 'https://yelpcampoo.onrender.com/auth/google'],
             defaultSrc: [],
             connectSrc: ["'self'", ...connectSrcUrls],
             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
